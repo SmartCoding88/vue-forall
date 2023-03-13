d99ffa966240a34d70bc0002cc80c315626218de
khouloudAmamouHaddad <khouloud.amamouhaddad@gmail.com>
2023-03-13 01:23:35 +0100

    Components within components

--- main.js
+++ main.js
@@ -19,4 +19,18 @@ app.component('task', {
     template: '<li><slot></slot></li>'
 });
 
+app.component('task-list', {
+    template: '<div><task v-for="task in tasks">{{task.description}}</task></div>',
+    data() {
+        return {
+            tasks: [
+                { description: 'Go to the store', finished: true },
+                { description: 'Finish screencast', finished: false },
+                { description: 'Make donation', finished: false },
+                { description: 'Clean the room', finished: true }
+            ]
+        }
+    }
+});
+
 app.mount("#root");

cf0015c5f567feb2299761dcea062507f9ee4eeb
khouloudAmamouHaddad <khouloud.amamouhaddad@gmail.com>
2023-03-14 19:47:58 +0100

    Tabs Example

--- main.js
+++ main.js
@@ -66,4 +66,36 @@ app.component('modal',{
     `,   
 });
 
+app.component('tabs',{
+    template:`
+    <div>
+        <div class="tabs">
+            <ul>
+                <li class="is-active"><a>Pictures</a></li>
+                <li><a>Music</a></li>
+                <li><a>Videos</a></li>
+                <li><a>Documents</a></li>
+            </ul>
+        </div>
+        <div class="tabs-details">
+            <slot></slot>
+        </div>
+    </div>
+    `,
+    data(){
+        tabs:[]
+    },
+    created(){
+        this.tabs = this.$children;
+    }
+});
+app.component('tab', {
+    template:`
+        <div><slot></slot></div>
+    `,
+    props:{
+        name: {required: true}
+    }
+})
+
 app.mount("#root");
